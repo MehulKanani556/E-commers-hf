c0e335a472c5e7c04dabff57253f0d0c9359920d
brijesh4224 <brijesh.kalathiyainfotech@gmail.com>
2024-10-11 11:24:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Component/header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
