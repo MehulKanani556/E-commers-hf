3458ce71836c2f8165e7de680b0ec80923c9892c
ParthVaghasiyakal-072 <parthvaghasiya.kalthiyainfo@gmail.com>
2024-10-16 18:02:45 +0530

    Parth16-10

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 import Womenstore from './darshan/pages/Womenstore';
 import Home from './Vivek/Component/Home/Home'
-import Electronics from './Parth/Component/Electronics/Electronics';
+// import Electronics from './Parth/Component/Electronics/Electronics';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Beauty from './Parth/Component/Beauty/Beauty';
 
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/darshan' element={<Womenstore />} />
-          <Route path='/parth' element={<Electronics />} />
+          <Route path='/parth' element={<Beauty />} />
           <Route path='/vivek' element={<Home />} />
         </Routes>
       </BrowserRouter>
